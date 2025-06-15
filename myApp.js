612f3ebf1d7da3038f986bfd52867fbee3b579c1
holiver32 <holiver.hurtado7@gmail.com>
2025-06-15 22:03:12 +0200

    add new practice Content Security Policy.

--- myApp.js
+++ myApp.js
@@ -6,8 +6,24 @@ app.use(helmet.frameguard({action:'deny'}));
 app.use(helmet.xssFilter({}));
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
+var ninetyDaysInSeconds = 90*24*60*60*1000;
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force:true}));
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({directives: {defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"] }}));
+app.use(helmet({ 
+  frameguard: {
+    action: 'deny'
+  },
 
-
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ['style.com'],
+    }
+  },
+   dnsPrefetchControl: false  
+}))
 
 
 
